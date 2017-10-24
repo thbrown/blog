5a82a4162f6fb268ab1e29234d9afd4ecc556360
Thomas Brown <thbrownmines@gmail.com>
2017-10-23 22:59:34 -0600

    Add basic input fields and a proof of concept slider in Angular Material.

--- retirement/src/app/app.component.ts
+++ retirement/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatSliderChange } from '@angular/material/slider'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,86 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  retirementContributions: number = 20000;
+  currentRetirementSavings: number = 50000;
+  expenses: number = 35000;
+
+  returnOnInvestment: number = 7;
+  returnOnInvestmentMin: number = 0;
+  returnOnInvestmentMax: number = 15;
+  returnOnInvestmentStep: number = 1;
+  returnOnInvestmentTickInterval: number = 1;
+  inflation: number = 3;
+
+  moneyToRetire: number;
+  moneyToRetireFormatted: string;
+  yearsToRetirement: number;
+  yearsToRetirementFormatted: string;
+
+  constructor() {
+    this.calculateOutput();
+   }
+
+  onRetirementContributionsInputChange(event): void {
+    // TODO(Lauren): Add slider.
+    this.calculateOutput();
+  }
+
+  onExpensesInputChange(event): void {
+    // TODO(Lauren): Add slider.
+    this.calculateOutput();
+  }
+
+  onCurrentRetirementSavingsInputChange(event): void {
+    this.calculateOutput();
+  }
+
+  onReturnOnInvestmentSliderChange(event): void {
+    this.returnOnInvestment = event.value;
+    this.calculateOutput();
+  }
+
+  onReturnOnInvestmentInputChange(event): void {
+    if (this.returnOnInvestment < this.returnOnInvestmentMin) {
+      this.returnOnInvestment = this.returnOnInvestmentMin;
+    }
+    if (this.returnOnInvestment > this.returnOnInvestmentMax) {
+      this.returnOnInvestment = this.returnOnInvestmentMax;
+    }
+    this.calculateOutput();
+  }
+
+  onInflationInputChange(event): void {
+    this.calculateOutput();
+  }
+
+  calculateOutput(): void {
+    this.moneyToRetire = this.expenses * 25;
+    this.moneyToRetireFormatted = '$' + this.formatMoney(this.moneyToRetire);
+    this.yearsToRetirement = 
+      this.nper(
+        (this.returnOnInvestment - this.inflation) / 100.0,
+        -1 * this.retirementContributions,
+        -1 * this.currentRetirementSavings,
+        this.moneyToRetire
+      );
+    this.yearsToRetirementFormatted = this.yearsToRetirement.toFixed(2);
+    if (this.yearsToRetirementFormatted === 'NaN') {
+      this.yearsToRetirementFormatted = 'You will never retire!!!';
+    }
+  }
+
+  nper(rate: number, paymentAmount: number, presentValue: number, futureValue: number) {
+    const endOrBeginning: number = 0;
+    let numerator: number = paymentAmount * (1 + rate * endOrBeginning) - futureValue * rate;
+    let denominator = (presentValue * rate + paymentAmount * (1 + rate * endOrBeginning));
+    return Math.log(numerator / denominator) / Math.log(1 + rate);
+  }
+
+  formatMoney(n): string {
+    return n.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+  }
+
+  title = 'Lauren';
 }
--- retirement/src/app/app.module.ts
+++ retirement/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule,MatFormFieldModule, MatInputModule, MatSliderModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
